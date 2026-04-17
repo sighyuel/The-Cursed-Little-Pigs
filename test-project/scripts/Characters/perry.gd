1f0c560f8aed2432f33d80f6a3cad2d7392a7966
Siul <siulgarcia18@gmail.com>
2026-04-17 19:43:00 -0400

    Perry affected by Wind
    - Added Wind Velocity
    - Laid out ground work for Dale blocking wind

--- test-project/scripts/Characters/perry.gd
+++ test-project/scripts/Characters/perry.gd
@@ -1,10 +1,13 @@
 extends character
 
 func _physics_process(delta: float) -> void:
+	velocity += wind_velocity * 0.4 
 	move_and_slide()
 	rect.visible = false
-	if not is_on_floor():
+	if not is_on_floor() && wind_velocity.y == 0.0:
 		velocity.y += gravity * delta
+	elif wind_velocity.y >0.0:
+		delta = 0
 #when perry extends, if this goes below to -1, it messes up the move_dir
 	if acceleration <= .5:
 		acceleration = .5
