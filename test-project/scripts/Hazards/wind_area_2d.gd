cb400c7f2e82ea8aeb55e42d2783f91c52a1de32
Siul <siulgarcia18@gmail.com>
2026-04-19 01:52:56 -0400

    Made Dale Disable Windbox
     - Whenever Dale walks into a Windbox the Windbox stops working
    - Whenever the Windbox is enabled and another character is in it they have to re-enter the Windbox to get pushed

--- test-project/scripts/Hazards/wind_area_2d.gd
+++ test-project/scripts/Hazards/wind_area_2d.gd
@@ -2,16 +2,17 @@ extends Area2D
 class_name WindArea2d
 
 @export var speed: Vector2 = Vector2.UP
+var save_speed: Vector2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_object_entered)
 	body_exited.connect(_object_exited)
+	save_speed = speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	print(speed)
 	
 
 func _object_entered(object: Node2D) -> void:
@@ -19,7 +20,12 @@ func _object_entered(object: Node2D) -> void:
 		if speed.y < 0.0:
 			object.velocity.y = speed.y
 		object.wind_velocity += speed
+	if object.name == "Dale":
+		speed = Vector2(0.0, 0.0)
 
 func _object_exited(object: Node2D) -> void:
+	if object.name == "Dale":
+		speed = save_speed
 	if "wind_velocity" in object:
 		object.wind_velocity -= speed
+		object.velocity.x = 0.0
