987f1f6266e87ed19c55c857d230f708bd6d57d8
EthanAriza <ethanariza006@gmail.com>
2026-04-30 14:54:21 -0400

    Credit and Start screen
    Credits a little messy but good for now

--- test-project/MainMeun.gd
+++ test-project/MainMeun.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-#
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +18,8 @@ func _on_start_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://credit_screen.tscn")
