d3a536d0d38c59a5cc9329efb504d054715b6a18
EthanAriza <ethanariza006@gmail.com>
2026-04-30 19:09:58 -0400

    Title and Credit Screen
    Both are done with an option button

---
+++ test-project/test-stuff/scenes/full_screen_control.gd
@@ -0,0 +1,11 @@
+extends CheckBox
+
+
+
+
+
+func _on_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
---
+++ test-project/test-stuff/scripts/MainMeun.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var button_manager: Control = $Button_manager
+@onready var options_box: Panel = $OptionsBox
+
+func _ready():
+	button_manager.visible = true
+	options_box.visible = false
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://test-stuff/levels/test-level.tscn")
+
+
+func _on_options_pressed() -> void:
+	print("Settings pressed")
+	button_manager.visible = false
+	options_box.visible = true
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	_ready()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://credit_screen.tscn")
