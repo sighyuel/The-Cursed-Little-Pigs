be55404855034d1aca7f10308cad22c6b09f35c7
komo <157924434+kitrous@users.noreply.github.com>
2026-05-07 16:36:54 -0400

    changed debug_reset button
    changed to "R"

--- test-project/test-stuff/levels/test_level.gd
+++ test-project/test-stuff/levels/test_level.gd
@@ -4,5 +4,5 @@ func _ready() -> void:
 	$Collectibles/CollectibleKey._coin_count = 0
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("debug_reset"):
 		get_tree().call_deferred("reload_current_scene")
