cfa08514bc6b861ecfcc9114ebd702f42696b627
EthanAriza <ethanariza006@gmail.com>
2026-05-08 15:33:25 -0400

    Little bit of fix

--- test-project/pause_menu.gd
+++ test-project/pause_menu.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
-func _input(event:InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Pause"):
 		if get_tree().paused:
 			visible = false 
