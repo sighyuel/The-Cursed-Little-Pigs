66b89442fc02f12cd6cbed708c71189d6f1ea4fe
sighyuel <siulgarcia18@gmail.com>
2026-05-08 03:17:40 -0400

    Made Level Select Button in Main Menu
    - Made Level Select Button that Leads to each room

--- test-project/test-stuff/scripts/MainMenu.gd
+++ test-project/test-stuff/scripts/MainMenu.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var button_manager: Control = $Button_manager
 @onready var options_box: Panel = $OptionsBox
+@onready var level_select: Panel = $Panel
 
 func _ready():
 	button_manager.visible = true
@@ -12,7 +13,6 @@ func _on_start_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	print("Settings pressed")
 	button_manager.visible = false
 	options_box.visible = true
 
@@ -25,3 +25,16 @@ func _on_back_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://test-stuff/scenes/credit_screen.tscn")
+
+
+func _on_level_select_pressed() -> void:
+	button_manager.visible = false
+	level_select.visible = true
+
+
+func _on_dale_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level 1/Character Rooms/dale's_room_level_1.tscn")
+
+
+func _on_may_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Level 1/Character Rooms/may's_room_level_1.tscn")
