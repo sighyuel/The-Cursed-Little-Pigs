71cd10a1780bb524841d8ec5dae7eb0a10064f3c
Siul <siulgarcia18@gmail.com>
2026-05-13 12:33:41 -0400

    Fixed Moving Platform Bug
    - Moving Platform no longer adds velocity to player
    
    - Changed tile size for TileMapLayer in Dale's room
    
    - Made Dale's Level beatable

--- test-project/scripts/Level Objects/moving_platform.gd
+++ test-project/scripts/Level Objects/moving_platform.gd
@@ -3,9 +3,6 @@ extends StaticBody2D
 @onready var animate = $AnimationPlayer
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if "platform_speed" in body:
-		pass
-		#body.platform_speed = animate.speed_scale
 	animate.play("platform_moving")
 
 
