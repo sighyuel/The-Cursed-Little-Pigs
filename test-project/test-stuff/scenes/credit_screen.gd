b983a8f17b0c064bd3015d5c0788632be2987f4f
EthanAriza <ethanariza006@gmail.com>
2026-05-15 15:17:51 -0400

    Litzy's Art
    Added Litzy's Art to the menu, credit screen, and pause menu

--- test-project/test-stuff/scenes/credit_screen.gd
+++ test-project/test-stuff/scenes/credit_screen.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 
 
-
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_INHERIT
+	process_mode = Node.PROCESS_MODE_PAUSABLE
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 
 func _on_button_pressed() -> void:
