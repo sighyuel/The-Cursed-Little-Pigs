e64d9a509d26c049defc1154d4fafd6f70cc1660
komo <157924434+kitrous@users.noreply.github.com>
2026-05-20 15:55:11 -0400

    menu polish
    -added tweens everytime you enter a button and returns back to its original valueeeeeeeeeeeeeeee (sorry im listening to a good song)
    
    -updated credits scene to being more accurate

--- test-project/test-stuff/scenes/quit_bounce_effect.gd
+++ test-project/test-stuff/scenes/quit_bounce_effect.gd
@@ -2,19 +2,15 @@ extends Button
 
 var tween: Tween
 
-func _ready() -> void:
-	grab_focus()
-	
-
 func _on_focus_entered() -> void:
 	reset_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(self,"scale", Vector2(0.20,0.22),0.4) # Replace with function body.
+	tween.tween_property(self,"position", Vector2(245,300),0.4) # Replace with function body.
 
 func _on_focus_exited() -> void:
 	reset_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(self,"scale",Vector2(0.2,0.2),0.4)
+	tween.tween_property(self,"position",Vector2(10,300),0.4)
 
 func reset_tween() -> void:
 	if tween:
