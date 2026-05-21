4ea4c55c5e4ba0deb9402c221470318f68b77f18
Siul <siulgarcia18@gmail.com>
2026-05-21 18:45:34 -0400

    Made last minute changes

--- test-project/scenes/Level 1/Character Rooms/PerryRoomToEndScreen.gd
+++ test-project/scenes/Level 1/Character Rooms/PerryRoomToEndScreen.gd
@@ -6,4 +6,4 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file.call_deferred("res://test-stuff/scenes/MainMenu.tscn")
+	get_tree().change_scene_to_file.call_deferred("res://scenes/Level 1/Character Rooms/dale's_room_level_1.tscn")
--- test-project/scripts/dale's_room_level_1.gd
+++ test-project/scripts/dale's_room_level_1.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var Phase1P = $Hazards/Pogo
 @onready var Phase2G =  $"Tile Map Layers Phase 2/Ground Phase 2"
 @onready var Phase2TP = $"Level Objects/Teleporter"
 @onready var tp_anim = $"Level Objects/Teleporter/AnimatedSprite2D"
@@ -15,6 +16,8 @@ func _process(delta: float) -> void:
 		get_tree().call_deferred("reload_current_scene")
 
 func _coin_collect() -> void:
+	Phase1P.visible = false
+	Phase1P.set_collision_mask_value(2, false)
 	Phase2G.enabled = true
 	Phase2TP.set_collision_mask_value(2, true)
 	tp_anim.play("default")
